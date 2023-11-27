52db27f98117fbfb4d2acf12c4827318e7b8c44e
daviardev <jerson.silva99@hotmail.com>
2023-11-27 12:33:32 -0500

    Configuration datatable

---
+++ js/config-table.js
@@ -0,0 +1,37 @@
+export const dataTableConfig = {
+  language: {
+    lengthMenu: 'Mostrar _MENU_ registros por página',
+    zeroRecords: 'Ningún dato ha sido encontrado',
+    info: 'Mostrando de _START_ a _END_ de un total de _TOTAL_ registros',
+    infoEmpty: 'No se encontró ningún registro',
+    infoFiltered: '(filtrados desde _MAX_ registros totales)',
+    search: 'Buscar',
+    loadingRecords: 'Cargando registros',
+    paginate: {
+      first: 'Primero',
+      last: 'Último',
+      next: 'Siguiente',
+      previous: 'Anterior'
+    }
+  },
+  dom: 'B<"clear">lfrtip',
+  buttons: {
+    buttons: [
+      {
+        extend: 'excel',
+        text: '<i class="fa-solid fa-table" style="color: #ffffff;"></i>',
+        className: 'btn btn-success mx-1 rounded'
+      },
+      {
+        extend: 'pdf',
+        text: '<i class="fa-solid fa-file-pdf" style="color: #ffffff;"></i>',
+        className: 'btn btn-danger mx-1 rounded'
+      },
+      {
+        extend: 'print',
+        text: '<i class="fa-solid fa-print" style="color: #ffffff;"></i>',
+        className: 'btn mx-1 rounded'
+      }
+    ]
+  }
+}
